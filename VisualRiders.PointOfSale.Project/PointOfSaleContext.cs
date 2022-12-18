3eabae2f9183298c027ea44ecae694beea471866
gzili <gustas.zilinskas@gmail.com>
2022-12-18 11:46:59 +0200

    set up DbContext and SQLite connection

---
+++ VisualRiders.PointOfSale.Project/PointOfSaleContext.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VisualRiders.PointOfSale.Project.Models;
+
+namespace VisualRiders.PointOfSale.Project;
+
+public class PointOfSaleContext : DbContext
+{
+    public PointOfSaleContext(DbContextOptions<PointOfSaleContext> options) : base(options) {}
+    
+    public DbSet<BusinessEntity> BusinessEntities { get; set; }
+    
+    public DbSet<Category> Categories { get; set; }
+    
+    public DbSet<Client> Clients { get; set; }
+    
+    public DbSet<ClientLoyalty> ClientLoyalties { get; set; }
+    
+    public DbSet<Discount> Discounts { get; set; }
+    
+    public DbSet<DiscountItem> DiscountItems { get; set; }
+    
+    public DbSet<ItemSelection> ItemSelections { get; set; }
+    
+    public DbSet<ItemSelectionValue> ItemSelectionValues { get; set; }
+    
+    public DbSet<Loyalty> Loyalties { get; set; }
+    
+    public DbSet<Order> Orders { get; set; }
+    
+    public DbSet<OrderItem> OrderItems { get; set; }
+    
+    public DbSet<Payment> Payments { get; set; }
+    
+    public DbSet<Product> Products { get; set; }
+    
+    public DbSet<ProductSelector> ProductSelectors { get; set; }
+    
+    public DbSet<Reservation> Reservations { get; set; }
+    
+    public DbSet<ReturnedItem> ReturnedItems { get; set; }
+    
+    public DbSet<Service> Services { get; set; }
+    
+    public DbSet<ServiceSelector> ServiceSelectors { get; set; }
+    
+    public DbSet<Shift> Shifts { get; set; }
+    
+    public DbSet<StaffMember> StaffMembers { get; set; }
+    
+    public DbSet<Tax> Taxes { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<ClientLoyalty>()
+            .HasKey(e => new { e.ClientId, e.LoyaltyId });
+        
+        modelBuilder.Entity<ReturnedItem>()
+            .HasKey(e => new { e.OrderItemId, e.PaymentId });
+    }
+}
--- VisualRiders.PointOfSale.Project/Program.cs
+++ VisualRiders.PointOfSale.Project/Program.cs
@@ -1,8 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using VisualRiders.PointOfSale.Project;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+builder.Services.AddDbContext<PointOfSaleContext>(
+    options => options.UseSqlite(builder.Configuration.GetConnectionString("Sqlite")));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
