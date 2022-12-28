b728cd60bfcf8001fb10f7e01723c01ecbbaba24
gzili <gustas.zilinskas@gmail.com>
2022-12-28 12:55:42 +0200

    implement business-focused inventory API

---
+++ VisualRiders.PointOfSale.Project/Services/InventoryService.cs
@@ -0,0 +1,46 @@
+﻿using AutoMapper;
+using VisualRiders.PointOfSale.Project.DTOs;
+using VisualRiders.PointOfSale.Project.Models;
+using VisualRiders.PointOfSale.Project.Repositories;
+
+namespace VisualRiders.PointOfSale.Project.Services;
+
+public class InventoryService
+{
+    private readonly InventoryRepository _inventoryRepository;
+    private readonly IMapper _mapper;
+
+    public InventoryService(InventoryRepository inventoryRepository, IMapper mapper)
+    {
+        _inventoryRepository = inventoryRepository;
+        _mapper = mapper;
+    }
+
+    public ReadInventoryDto? GetByProductId(int productId)
+    {
+        var inventory = _inventoryRepository.GetByProductId(productId);
+
+        return _mapper.Map<ReadInventoryDto>(inventory);
+    }
+
+    public ReadInventoryDto Update(int productId, UpdateInventoryDto dto)
+    {
+        var inventory = _inventoryRepository.GetByProductId(productId);
+
+        if (inventory != null)
+        {
+            _mapper.Map(dto, inventory);
+        }
+        else
+        {
+            inventory = _mapper.Map<Inventory>(dto);
+            inventory.BusinessEntityId = 1;
+            inventory.ProductId = productId;
+            _inventoryRepository.Add(inventory);
+        }
+        
+        _inventoryRepository.SaveChanges();
+
+        return _mapper.Map<ReadInventoryDto>(inventory);
+    }
+}
